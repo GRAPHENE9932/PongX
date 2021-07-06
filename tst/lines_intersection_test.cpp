f80144251a254bc7d9c89572ddd513132403d04a
GRAPHENE9932 <artemklim50@gmail.com>
2021-07-06 15:55:03 +0300

    Add tests for horizontal lines intersection

--- tst/lines_intersection_test.cpp
+++ tst/lines_intersection_test.cpp
@@ -16,12 +16,47 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <limits>
+
 #include <gtest/gtest.h>
 
 #include "../src/game_math.hpp"
 
+#define INF_F std::numeric_limits<float>().infinity()
+//Macros for sf::Vector
+#define EXPECT_EQ_V2(exp, orig) \
+	EXPECT_EQ(exp.x, orig.x); \
+	EXPECT_EQ(exp.y, orig.y);
+#define EXPECT_NEAR_V2(exp, orig, abs_error) \
+	EXPECT_NEAR(exp.x, orig.x, abs_error); \
+	EXPECT_NEAR(exp.y, orig.y, abs_error);
+
 TEST(lines_intersection, horizontal) {
-	EXPECT_EQ(0, 0);
+	sf::Vector2f tmp_result;
+	//y=x and y=0 lines
+	EXPECT_EQ(true, gm::hor_segment_line_intersection(1.0F, {0, 0}, -1.0F, 1.0F, 0.0F, tmp_result));
+	EXPECT_EQ_V2(sf::Vector2f(0.0F, 0.0F), tmp_result);
+
+	//y=0,5x and y=0 lines
+	EXPECT_EQ(true, gm::hor_segment_line_intersection(0.5F, {0, 0}, -1.0F, 1.0F, 0.0F, tmp_result));
+	EXPECT_EQ_V2(sf::Vector2f(0.0F, 0.0F), tmp_result);
+
+	//y=x and line segment from -15 to 5
+	EXPECT_EQ(false, gm::hor_segment_line_intersection(1.0F, {0, 0}, -15.0, -5.0, 0.0F, tmp_result));
+
+	//Similar, but more random test
+	EXPECT_EQ(true, gm::hor_segment_line_intersection(3.5F, {0, 1}, 0.0F, 2.0F, 5.0F, tmp_result));
+	EXPECT_NEAR_V2(sf::Vector2f(1.14286F, 5.0F), tmp_result, 0.00001F);
+
+	//Same, but with other line segment points
+	EXPECT_EQ(false, gm::hor_segment_line_intersection(3.5F, {0, 1}, -2.0F, 0.0F, 5.0F, tmp_result));
+
+	//Some crazy tests with infinity
+	EXPECT_EQ(true, gm::hor_segment_line_intersection(INF_F, {0, 0}, -1.0F, 1.0F, 0.0F, tmp_result));
+	EXPECT_EQ_V2(sf::Vector2f(0.0F, 0.0F), tmp_result);
+
+	EXPECT_EQ(true, gm::hor_segment_line_intersection(INF_F, {0, 0}, -1.0F, 1.0F, 5.0F, tmp_result));
+	EXPECT_EQ_V2(sf::Vector2f(0.0F, 5.0F), tmp_result);
 }
 
 TEST(lines_intersection, vertical) {
